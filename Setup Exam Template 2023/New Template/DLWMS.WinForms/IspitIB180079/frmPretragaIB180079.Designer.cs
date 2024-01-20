8acd47cc93d8394eeeb7a5c2d09a7803077f6a15
Vedad <vedad.ribery@gmail.com>
2024-01-21 00:59:38 +0100

    Dodavanje novog templejta

---
+++ Setup Exam Template 2023/New Template/DLWMS.WinForms/IspitIB180079/frmPretragaIB180079.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace DLWMS.WinForms.IspitIB180079
+{
+    partial class frmPretragaIB180079
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmPretragaIB180079
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(802, 328);
+            this.Name = "frmPretragaIB180079";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pretraga";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Setup Exam Template 2023/New Template/DLWMS.WinForms/IspitIB180079/frmPretragaIB180079.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DLWMS.WinForms.IspitIB180079
+{
+    public partial class frmPretragaIB180079 : Form
+    {
+        public frmPretragaIB180079()
+        {
+            InitializeComponent();
+        }
+    }
+}
